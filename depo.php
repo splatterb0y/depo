b5125e538dffda2e7ca78fc9fe4fa0c0e2b277d6
Christian Ziegenrücker <splatterb0y@hotmail.de>
2012-07-31 00:24:49 +0200

    Massiv cleanup

--- depo.php
+++ depo.php
@@ -3,21 +3,22 @@
 /**
  * @author Christian Ziegenrücker
  * @author Sebastian Langer
- * @copyright http://softlevel.de
  *  
- * @version 0.1
+ * @version 0.2
  *
  * DePo - Drupal Deploy based on Repo
  * 
- * @todo implementieren version nachtragen für module
+ * @todo checkout of single/multiple projects but not the whole manifest
+ *          repo sync modules/xmlsitemap .. .. 
  * @todo drush befehle ausführen
- * @todo Repo Refs fixen, damit pushen auch möglich ist
+ * @todo Repo Refs fixen, damit pushen auch möglich ist(?)
  */
 
 if ( !isset($argv[1]) ) {
     echo "\033[31m No parameters given.\033[0m".PHP_EOL;
 } 
 else {
+    echo "\033[1;37m Repo started...\033[0m".PHP_EOL;
     switch ( $argv[1] ) {
         case 'init':
                 _init();
@@ -37,7 +38,6 @@ function _init() {
     } 
     else {
 
-        echo "\033[1;37m Repo started...\033[0m".PHP_EOL;
         passthru('repo init -u '.$argv[2], &$return);
 
         if ($return == 0) {
@@ -52,38 +52,37 @@ function _init() {
 
 function _sync() {
     
-    echo "\033[1;37m Repo started...\033[0m".PHP_EOL;
-    
     passthru('repo sync');
     
     try {
-                $manifest = new SimpleXmlElement('file://'.getcwd().'/.repo/manifest.xml', NULL, TRUE);  
-                
-                
-                foreach ( $manifest as $project ) {
-                    //If patch child available start patcher.                
-                    if ($project->patch) {
-                        require_once(dirname(__FILE__).'/libs/patcher.inc');
-                        patcher($project);
-                    }
-                    if ( $project->download ) {
-                        require_once(dirname(__FILE__).'/libs/downloader.inc');
-                        downloader($project);
-                    }
-                     
-                    if ( isset($project['git-version']) && $project['git-version'] == true ) {
-                        require_once(dirname(__FILE__).'/libs/versioner.inc');
-                        versioner($project);                        
-                    }
-                                        
-                }
-                
-                unset( $manifest );
+        $manifest = new SimpleXmlElement('file://' . getcwd() . '/.repo/manifest.xml', NULL, TRUE); 
+        
+        foreach ( $manifest->project as $project ) {
+
+            echo ":: ".$project['name'].PHP_EOL;
+
+            if ( $project->patch ) {
+                require_once(dirname(__FILE__).'/libs/patcher.inc');
+                patcher($project);
             }
-            catch ( Exception $ex ) {
-                echo "\033[31m Something went wrong while reading the manifest.\033[0m".PHP_EOL;
-                echo $ex;
+            
+            if ( $project->download ) {
+                require_once(dirname(__FILE__).'/libs/downloader.inc');
+                downloader($project);
             }
+
+            if ( isset($project['git-version']) && $project['git-version'] == true ) {
+                require_once(dirname(__FILE__).'/libs/versioner.inc');
+                versioner($project);                        
+            }                                        
+        }
+
+        unset( $manifest );
+    }
+    catch ( Exception $ex ) {
+        echo "\033[31m Something went wrong while reading the manifest.\033[0m".PHP_EOL;
+        echo $ex;
+    }
     
 }
 ?>
